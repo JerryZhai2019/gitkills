59f48f59040b20a9c08688de9d2ea0ac19d418b6
Andy <zhairuiying@sansi.com>
2021-10-19 14:26:02 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
+def login(request)
+    return redirect('/index')
 
 def index(request):
     return HttpResponse('CSDN读者你们好')
